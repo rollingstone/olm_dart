b2b1504c62ca96a7ae54cd9dab065017e0951a37
Lukas Lihotzki <lukas@lihotzki.de>
2020-02-24 14:18:19 +0100

    Improve autojs codegen with jq

--- codegen/codegen.sh
+++ codegen/codegen.sh
@@ -3,5 +3,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 cd "$(dirname "$0")"/..
-codegen/autojs.sh < lib/native.dart > lib/js.dart
+codegen/autojs.jq < lib/native.dart > lib/js.dart
 echo '#include "olm/olm.h"' | gcc -I native/olm/include -E - | sed -n ':a;/^#.*\Wolm\//{:b;n;/#/ba;/./p;bb}' | codegen/autoffi.jq > lib/src/ffi.dart
