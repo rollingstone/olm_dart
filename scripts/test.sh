3b5b95efd0ba617aa7f017605021271589999bdb
Lukas Lihotzki <lukas@lihotzki.de>
2021-03-10 15:46:19 +0100

    test: remove test_coverage package
    Line coverage changes from 830 of 839 to 823 of 832, because of
    https://github.com/dart-lang/test/issues/1403.

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,9 +4,10 @@
 
 cd "$(dirname "$0")"/..
 [ -d native/olm ] && export LD_LIBRARY_PATH=$(pwd)/native/olm
-pub run test -p vm,firefox
-pub run test_coverage
+pub run test -p vm,firefox --coverage coverage
+pub run coverage:format_coverage --packages .packages --report-on lib/ -l -o coverage/lcov.info -i coverage
 genhtml -o coverage coverage/lcov.info || scripts/lcov_stats.sh coverage/lcov.info || true
 
-dart2native test/.test_coverage.dart
-valgrind --show-mismatched-frees=no --exit-on-first-error=yes --error-exitcode=1 test/.test_coverage.exe
+mkdir -p build
+dart2native test/olm_test.dart -o build/olm_test.exe
+valgrind --show-mismatched-frees=no --exit-on-first-error=yes --error-exitcode=1 build/olm_test.exe
